0366e6d188efbdfbb72ed9f4c360a45c1077855b
roti0 <albariiqy9@gmail.com>
2019-06-10 12:47:05 +0700

    added databse

--- home.php
+++ home.php
@@ -1,1 +1,61 @@
-<h1>WELCOME</h1>
+<!DOCTYPE html>
+<?php
+session_start();
+include("includes/header.php");
+
+if (!isset($_SESSION['username'])) {
+    header("location: index.php");
+}
+?>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <?php
+    $usr = $_SESSION["username"];
+    $get_user = "SELECT * FROM users where username='$usr'";
+    $run_user = mysqli_query($conn, $get_user);
+    $row = mysqli_fetch_array($run_user);
+
+    $username = $row['username'];
+
+    ?>
+
+    <title><?= $username; ?></title>
+    <!-- Font Awesome Icons -->
+    <!-- Latest compiled and minified CSS -->
+    <!-- Latest compiled and minified JavaScript -->
+
+    <link href="style/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+
+    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    
+    <link rel="stylesheet" type="text/css" href="style/css/home_style2.css">
+
+
+</head>
+
+<body>
+<div class="row">
+	<div id="insert_post" class="col-sm-12">
+		<center>
+		<form action="home.php?id=<?= $user_id; ?>" method="post" id="f" enctype="multipart/form-data">
+		<textarea class="form-control" id="content" rows="4" name="content" placeholder="What's in your mind?"></textarea><br>
+		<label class="btn btn-warning" id="">Select Image
+		<input type="file" name="upload_image" size="30">
+		</label>
+		<button id="btn-post" class="btn btn-success" name="sub">Post</button>
+		</form>
+		
+		</center>
+	</div>
+</div>
+</body>
+
+</html>
--- includes/header.php
+++ includes/header.php
@@ -1,21 +1,21 @@
 <?php
 include 'connections.php';
 ?>
-
 <nav class="navbar navbar-default">
     <div class="container-fluid">
         <div class="navbar-header">
-            <button type="button" data-toggle="collapse" class="navbar-toggle collapsed" aria-expanded="false" data-target="#bs-example-navbar-collapse-1">
-            
-            <span class="sr-only">Toggle navigatioin</span>
-            <span class="iconbar"></span>
-            <span class="iconbar"></span>
-            <span class="iconbar"></span>
+            <button type="button" class="navbar-toggle collapsed" data-target="#bs-example-navbar-collapse-1" data-toggle="collapse" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
             </button>
-            <a href="home.php" class="navbar-brand">SosmedC</a>
+            <a class="navbar-brand" href="home.php">Coding Cafe</a>
         </div>
-        <div class="collapse navbar-collapse" id="#bs-example-navbar-collapse-1">
+
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
+
                 <?php
                 $usr = $_SESSION['username'];
                 $get_user = "SELECT * FROM users where username='$usr'";
@@ -43,34 +43,40 @@ include 'connections.php';
                 $posts = mysqli_num_rows($run_posts);
                 ?>
 
-                <li><a href="profile.php?<?php echo 'u_id=$userid' ?>"> <?php echo $name; ?> </a></li>
+                <li><a href='profile.php?<?php echo "u_id=$userid" ?>'><?php echo "$name"; ?></a></li>
                 <li><a href="home.php">Home</a></li>
-                <li><a href="home.php">Find People</a></li>
+                <li><a href="members.php">Find People</a></li>
                 <li><a href="messages.php?u_id=new">Messages</a></li>
-                <li class='dropdown'>
-                    <a href='#' class='dropdown-toggle' data-toggle='dropdown' role='button' aria-haspopup='true' aria-expanded='false'>
-                        <i> <span class='glyphicon glyphicon-chevron-down'></i></span></a>
-                    <ul class='dropdown-menu'>
-                        <li>
-                            <a href='my_posts.php?u_id=<? $userid; ?>'>My Posts<span class='badge badge-secondary'><?= $posts ?></span></a>
-                        </li>
-                        <li>
-                            <a href='edit_profile.php?u_id=<?=$userid?>'>Edit Account</a>
-                        </li>
-                        <li role='separator' class='decider'></li>
-                        <li>
-                            <a href='logout.php'>Logout</a>
-                        </li>
-                    </ul>
-                </li>
+
+
+                <?php
+                echo "
+
+						<li class='dropdown'>
+							<a href='#' class='dropdown-toggle' data-toggle='dropdown' role='button' aria-haspopup='true' aria-expanded='false'><span><i class='glyphicon glyphicon-chevron-down'></i></span></a>
+							<ul class='dropdown-menu'>
+								<li>
+									<a href='my_post.php?u_id=$userid'>My Posts <span class='badge badge-secondary'>$posts</span></a>
+								</li>
+								<li>
+									<a href='edit_profile.php?u_id=$userid'>Edit Account</a>
+								</li>
+								<li role='separator' class='divider'></li>
+								<li>
+									<a href='logout.php'>Logout</a>
+								</li>
+							</ul>
+						</li>
+						";
+                ?>
             </ul>
             <ul class="nav navbar-nav navbar-right">
                 <li class="dropdown">
-                    <form action="results.php" method="GET" class="navbar-form nafbar-left">
+                    <form class="navbar-form navbar-left" method="get" action="results.php">
                         <div class="form-group">
-                            <input type="text" name="user_query" class="form-control" placeholder="Search">
-                            <button type="submit" class="btn btn-info" name="search">Search</button>
+                            <input type="text" class="form-control" name="user_query" placeholder="Search">
                         </div>
+                        <button type="submit" class="btn btn-info" name="search">Search</button>
                     </form>
                 </li>
             </ul>
